b52ffb2cf07c3acdf62ba76e6ca28efc4b0f41ab
somya Kumari <somya.kumari@stellium.com>
2024-11-25 13:10:43 +0000

    Initial commit

--- testPlugin/webapp/testPlugin/controller/MainView.controller.js
+++ testPlugin/webapp/testPlugin/controller/MainView.controller.js
@@ -3,16 +3,20 @@ sap.ui.define([
 	"sap/dm/dme/podfoundation/controller/PluginViewController",
 	"sap/ui/model/json/JSONModel",
     "sap/m/ColumnListItem",
-    "sap/m/Column",
-    "sap/m/Text",
+    // "sap/m/Column",
+    // "sap/m/Text",
     'sap/m/Label',
     'sap/ui/model/Filter',
     'sap/ui/model/FilterOperator',
     'sap/ui/comp/smartvariants/PersonalizableInfo',
     "sap/ui/core/Fragment",
     "sap/ui/core/Control",
-    'sap/ui/export/Spreadsheet' // Import the Spreadsheet library
-], function (jQuery, PluginViewController, JSONModel, ColumnListItem, Column, Text, Label, Filter, FilterOperator, PersonalizableInfo, Fragment, Control, Spreadsheet) {
+    'sap/ui/export/Spreadsheet', // Import the Spreadsheet library
+     "sap/ui/table/Column",
+     "sap/m/Text",
+     "sap/m/MessageBox",
+     "sap/m/MessageToast"
+], function (jQuery, PluginViewController, JSONModel, ColumnListItem, Label, Filter, FilterOperator, PersonalizableInfo, Fragment, Control, Spreadsheet, Column, Text, MessageBox, MessageToast) {
 	"use strict";
 
 	return PluginViewController.extend("company.custom.plugins.testPlugin.testPlugin.controller.MainView", {
@@ -26,38 +30,361 @@ sap.ui.define([
             
         
 		},
-        onSearch: function (oEvent) {
-            var oFilterBar = oEvent.getSource(),
-                aFilterGroupItems = oFilterBar.getFilterGroupItems(),
-                aFilters = [];
+        onMaterialValueHelpRequest: function() {
+            var oView = this.getView(),
+                oViewModel = oView.getModel("data");
+            
+            if (!this.oMaterialVHDia) {
+                //Load the fragment
+                this.oMaterialVHDia = sap.ui.xmlfragment(
+                    "company.custom.plugins.testPlugin.testPlugin.view.fragments.MaterialValueHelpRequest",
+                    this
+                );
 
+                this.oMaterialVHDia.getTableAsync().then(function (oTable) {
+                    //Add columns to the table
+                    oTable.addColumn(
+                        new Column({
+                            label: new Text({ text: "Material" }),
+                            template: new Text({ text: "{data>material/material}" }),
+                            width: "170px"
+
+                        })
+                    );
+                    oTable.addColumn(
+                        new Column({
+                            label: new Text({ text: "status" }),
+                            template: new Text({ text: "{data>material/description}" }),
+                            width: "180px"
+                        })
+                    );
+                    oTable.addColumn(
+                        new Column({
+                            label: new Text({ text: "orderType" }),
+                            template: new Text({ text: "{data>material/version}" }),
+                            width: "170px"
+                        })
+                    );
+                    //Bind data to the table
+                    oTable.setModel(oViewModel, "data");
+                    oTable.bindRows("data>/tabItems");
+                });
+            }
+            this.oMaterialVHDia.open();
+        },
+         
+        onMaterialVHDiaSearch: function (oEvent) {
+            var oFilterBar = oEvent.getSource(),
+            aFilterGroupItems = oFilterBar.getFilterGroupItems(),
+            aFilters = [];
+
+            //Create filters based on selected input Values
             aFilters = aFilterGroupItems.map(function (oFGI) {
                 var oControl = oFGI.getControl();
-                if (oControl && oControl.getSelectedItem) {
-                    var oSelectedItem = oControl.getSelectedItem();
-                    if (oSelectedItem) {
-                        var sSelectedText = oSelectedItem.getText();
-                        return new Filter({
-                            path: oFGI.getName(),
-                            operator: FilterOperator.EQ,
-                            value1: sSelectedText
-                        });
-                    }
+                if (oControl && oControl.getValue) {
+                    return new Filter({
+                        path: oFGI.getName(),
+                        operator: FilterOperator.Contains,
+                        value1: oControl.getValue()
+                    });
                 }
-                return null;
-            }).filter(function (oFilter) {
-                return oFilter !== null;
-            });
+            })
+            .filter(Boolean); //Filter out empty values
 
-            var oTable = this.getView().byId('table');
-            var oBinding = oTable.getBinding("items");
-            if (oBinding) {
-                oBinding.filter(aFilters);
-            } else {
-                console.error("Table binding is not available");
+            //Get the table for dialog and apply filter 
+            this.oMaterialVHDia.getTableAsync().then(oTable => {
+               var oRowBindingCtx = oTable.getBinding("rows");
+            //    oRowBindingCtx = oTable.getBinding("rows");
+               oRowBindingCtx.filter(aFilters);
+            });
+        },
+        onMaterialVHDiaOKPress: function (oEvent) {
+            var aSelectedItems = oEvent.getParameter("tokens");
+
+            //No order selected
+            if (aSelectedItems.length < 1) {
+                return;
             }
+
+            //Close dialog
+            this.oMaterialVHDia.close();
+
+            //Get Batch info for selected resource
+            var sSelectedmaterial = aSelectedItems[0].getKey();
+
+            //Set the selected order to model
+            var oViewModel = this.getView().getModel("data");
+            oViewModel.setProperty("/material/material", sSelectedmaterial);
+            oViewModel.setProperty("/ismaterialSelected", true);
+            console.log("sSelectedmaterial", sSelectedmaterial);
         },
 
+        
+        
+        
+
+            onMaterialVHDiaCancelPress: function (oEvent) {
+                this.oMaterialVHDia.close();
+            },
+        
+        onOrderValueHelpRequest: function() {
+            var oView = this.getView(),
+                oViewModel = oView.getModel("data");
+            
+            if (!this.oOrderVHDia) {
+                //Load the fragment
+                this.oOrderVHDia = sap.ui.xmlfragment(
+                    "company.custom.plugins.testPlugin.testPlugin.view.fragments.OrderValueHelpRequest",
+                    this
+                );
+
+                this.oOrderVHDia.getTableAsync().then(function (oTable) {
+                    //Add columns to the table
+                    oTable.addColumn(
+                        new Column({
+                            label: new Text({ text: "Order" }),
+                            template: new Text({ text: "{data>order}" }),
+                            width: "170px"
+
+                        })
+                    );
+                    oTable.addColumn(
+                        new Column({
+                            label: new Text({ text: "status" }),
+                            template: new Text({ text: "{data>status}" }),
+                            width: "170px"
+                        })
+                    );
+                    oTable.addColumn(
+                        new Column({
+                            label: new Text({ text: "orderType" }),
+                            template: new Text({ text: "{data>orderType}" }),
+                            width: "170px"
+                        })
+                    );
+                    //Bind data to the table
+                    oTable.setModel(oViewModel, "data");
+                    oTable.bindRows("data>/tabItems");
+                });
+            }
+            this.oOrderVHDia.open();
+        },
+         
+        onOrderVHDiaSearch: function (oEvent) {
+            var oFilterBar = oEvent.getSource(),
+            aFilterGroupItems = oFilterBar.getFilterGroupItems(),
+            aFilters = [];
+
+            //Create filters based on selected input Values
+            aFilters = aFilterGroupItems.map(function (oFGI) {
+                var oControl = oFGI.getControl();
+                if (oControl && oControl.getValue) {
+                    return new Filter({
+                        path: oFGI.getName(),
+                        operator: FilterOperator.Contains,
+                        value1: oControl.getValue()
+                    });
+                }
+            })
+            .filter(Boolean); //Filter out empty values
+
+            //Get the table for dialog and apply filter 
+            this.oOrderVHDia.getTableAsync().then(oTable => {
+               var oRowBindingCtx = oTable.getBinding("rows");
+            //    oRowBindingCtx = oTable.getBinding("rows");
+               oRowBindingCtx.filter(aFilters);
+            });
+        },
+        onOrderVHDiaOKPress: function (oEvent) {
+            var aSelectedItems = oEvent.getParameter("tokens");
+
+            //No order selected
+            if (aSelectedItems.length < 1) {
+                return;
+            }
+
+            //Close dialog
+            this.oOrderVHDia.close();
+
+            //Get Batch info for selected resource
+            var sSelectedorder = aSelectedItems[0].getKey();
+
+            //Set the selected order to model
+            var oViewModel = this.getView().getModel("data");
+            oViewModel.setProperty("/order", sSelectedorder);
+            oViewModel.setProperty("/isorderSelected", true);
+        },
+
+        
+        
+        
+
+            onOrderVHDiaCancelPress: function (oEvent) {
+                this.oOrderVHDia.close();
+            },
+            onStatusValueHelpRequest: function() {
+                var oView = this.getView(),
+                    oViewModel = oView.getModel("data");
+                
+                if (!this.oStatusVHDia) {
+                    //Load the fragment
+                    this.oStatusVHDia = sap.ui.xmlfragment(
+                        "company.custom.plugins.testPlugin.testPlugin.view.fragments.StatusValueHelpRequest",
+                        this
+                    );
+    
+                    this.oStatusVHDia.getTableAsync().then(function (oTable) {
+                        //Add columns to the table
+                        oTable.addColumn(
+                            new Column({
+                                label: new Text({ text: "Status" }),
+                                template: new Text({ text: "{data>status}" }),
+                                width: "170px"
+    
+                            })
+                        );
+                        oTable.addColumn(
+                            new Column({
+                                label: new Text({ text: "executionStatus" }),
+                                template: new Text({ text: "{data>executionStatus}" }),
+                                width: "170px"
+                            })
+                        );
+                        oTable.addColumn(
+                            new Column({
+                                label: new Text({ text: "releaseStatus" }),
+                                template: new Text({ text: "{data>releaseStatus}" }),
+                                width: "170px"
+                            })
+                        );
+                        //Bind data to the table
+                        oTable.setModel(oViewModel, "data");
+                        oTable.bindRows("data>/tabItems");
+                    });
+                }
+                this.oStatusVHDia.open();
+            },
+             
+            onStatusVHDiaSearch: function (oEvent) {
+                var oFilterBar = oEvent.getSource(),
+                aFilterGroupItems = oFilterBar.getFilterGroupItems(),
+                aFilters = [];
+    
+                //Create filters based on selected input Values
+                aFilters = aFilterGroupItems.map(function (oFGI) {
+                    var oControl = oFGI.getControl();
+                    if (oControl && oControl.getValue) {
+                        return new Filter({
+                            path: oFGI.getName(),
+                            operator: FilterOperator.Contains,
+                            value1: oControl.getValue()
+                        });
+                    }
+                })
+                .filter(Boolean); //Filter out empty values
+    
+                //Get the table for dialog and apply filter 
+                this.oStatusVHDia.getTableAsync().then(oTable => {
+                   var oRowBindingCtx = oTable.getBinding("rows");
+                //    oRowBindingCtx = oTable.getBinding("rows");
+                   oRowBindingCtx.filter(aFilters);
+                });
+            },
+            onStatusVHDiaOKPress: function (oEvent) {
+                var aSelectedItems = oEvent.getParameter("tokens");
+    
+                //No Status selected
+                if (aSelectedItems.length < 1) {
+                    return;
+                }
+    
+                //Close dialog
+                this.oStatusVHDia.close();
+    
+                //Get Batch info for selected resource
+                var sSelectedStatus = aSelectedItems[0].getKey();
+    
+                //Set the selected order to model
+                var oViewModel = this.getView().getModel("data");
+                oViewModel.setProperty("/status", sSelectedStatus);
+                oViewModel.setProperty("/isStatusSelected", true);
+            },
+    
+            
+            
+            
+    
+                onStatusVHDiaCancelPress: function (oEvent) {
+                    this.oStatusVHDia.close();
+                },
+                onSearch: function() {
+                    var oView = this.getView(),
+                        oViewModel = oView.getModel("data"),
+                        aFilters = [];
+                
+                    // Get selected Order
+                    var sSelectedOrder = oViewModel.getProperty("/order");
+                    if (sSelectedOrder) {
+                        aFilters.push(new sap.ui.model.Filter("order", sap.ui.model.FilterOperator.EQ, sSelectedOrder));
+                    }
+                
+                    // Get selected Material
+                    var sSelectedMaterial = oViewModel.getProperty("/material/material");
+                    if (sSelectedMaterial) {
+                        aFilters.push(new sap.ui.model.Filter("material/material", sap.ui.model.FilterOperator.EQ, sSelectedMaterial));
+                    }
+                
+                    // Get selected Status
+                    var sSelectedStatus = oViewModel.getProperty("/status");
+                    if (sSelectedStatus) {
+                        aFilters.push(new sap.ui.model.Filter("status", sap.ui.model.FilterOperator.EQ, sSelectedStatus));
+                    }
+                
+                    // Get selected Date Range (start and end dates)
+                    var oDateRange = oView.byId("dateRange").getDateValue();
+                    var oEndDate = oView.byId("dateRange").getSecondDateValue();
+                    console.log("oDateRange", oDateRange);
+                    console.log("oEndDate", oEndDate);
+
+
+                    
+                    // Apply Date Range filter if dates are selected
+                    if (oDateRange && oEndDate) {
+                        //Convert the dates to ISO format strins(e.g., "YYYY-MM-DD")
+                        var sStartDateISO = oDateRange.toISOString();
+                        var sEndDateISO = oEndDate.toISOString();
+
+                        
+                        aFilters.push(new sap.ui.model.Filter({
+                            path: "plannedStartDate",
+                            operator: sap.ui.model.FilterOperator.BT,
+                            value1: sStartDateISO,
+                            value2: sEndDateISO
+                        }));
+                    }
+                
+                    // Apply filters to the table's binding
+                    var oTable = oView.byId("table"),
+                        oBinding = oTable.getBinding("items");
+                        console.log("aFilters", aFilters);
+                        console.log("Table Binding before filters:", oBinding);
+
+                
+                    oBinding.filter(aFilters);
+                    oBinding.refresh(true);
+                    //Log table binding after filter to verify
+                    console.log("Table Binding after filter:", oBinding);
+                },
+                
+                
+                
+        
+
+        
+        
+        
+        
+    
         _fetchData: function () {
             var that = this;
             var sUrl = this.getPublicApiRestDataSourceUri() + '/order/v1/orders/list';
